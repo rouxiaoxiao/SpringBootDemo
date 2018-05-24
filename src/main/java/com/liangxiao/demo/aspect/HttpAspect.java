7e45d13149de045ce8970f002ca92049f6fe64d0
xiaoxiao <1621841044@qq.com>
2018-05-25 00:46:24 +0800

    AOP

---
+++ src/main/java/com/liangxiao/demo/aspect/HttpAspect.java
@@ -0,0 +1,14 @@
+package com.liangxiao.demo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author: liangxiao
+ * @Date: Created in 0:41 2018/5/25
+ */
+@Aspect
+@Component
+public class HttpAspect {
+
+}
